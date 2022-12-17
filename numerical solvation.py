0b61a589b0655ed2dc22f6d0e2d9ad548353e082
Egor Masov <113713144+eMasov@users.noreply.github.com>
2022-12-17 15:21:20 +0300

    Add files via upload

--- numerical solvation.py
+++ numerical solvation.py
@@ -7,7 +7,7 @@ A = 5 # generator amplitude
 gamma = 0.1 # vibration damping coefficient
 omega_2 = 1 # omega_2 = k / M
 
-M = np.eye(12) * gamma
+M = np.eye(12) *gamma
 
 for i in range(1, 12, 2):
     M[i][i - 1] = 1
@@ -26,11 +26,16 @@ M[10][11] = -2 * omega_2
 def deriv_u(u, t):
     return M @ u + np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, A * np.sin(omega_f * t), 0])
 
+
+# array of initial conditions (even - u_i, odd - u_i derivative)
 u0 = np.zeros(12)
+
 t = np.linspace(0, 80)
 u = odeint(deriv_u, u0, t)
 
 u1 = [u[i][1] for i in range(len(u))]
+
+# here the plot for first body
 plt.plot(t, u1)
 plt.xlabel('Time')
 plt.ylabel('U(t)')
